ef6561664cbd9c3b6737e2fbd0fbc3841ebabf4b
hp <pichaiku@gmail.com>
2023-01-20 05:59:13 +0700

    commit

---
+++ resources/views/product/show.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    
+    <h2>Bordered Table</h2>
+    <p>The .table-bordered class adds borders to a table:</p>            
+    <table style="border-style: dotted;width:100%">
+        <thead>
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+        </tr>
+        <tr>
+            <td>Mary</td>
+            <td>Moe</td>
+            <td>mary@example.com</td>
+        </tr>
+        <tr>
+            <td>July</td>
+            <td>Dooley</td>
+            <td>july@example.com</td>
+        </tr>
+        </tbody>
+    </table>
+    <style>
+    back
+    </style>   
+<div class="contrainer">
+    <div class="row">
+        <div class="col-sm-4" >ชื่อ</div>
+        <div class="col-sm-8">นามสกุล</div>
+    </div>
+</div>
+
+</div>
+
+</body>
+</html>
